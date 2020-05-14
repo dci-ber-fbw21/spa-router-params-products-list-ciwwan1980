b74fa9477e88bc206eedbb8d5ba3bbaf27ad55dd
Ajil Abdalqader <ajilqado@gmail.com>
2020-05-14 14:41:24 +0200

    importing-data-in-products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import data from "../data/products.json"
+import {Link} from "react-router-dom"
 
 export default function Products() {
+    console.log(data,"data")
     return (
         <div>
             <h1>products</h1>
+           
+           
+            {data.map(item=>{
+                return (
+                    <div key={item.id}>
+                    <p>{item.name}</p>
+                    <p>{item.shortDescription}</p>
+                    <p>{item.price}</p>
+                    </div>
+                )
+            })}
+         
         </div>
     )
 }
