c2c4e9adf968c5b3716bb0d2b042c432b9adf3bd
Ajil Abdalqader <ajilqado@gmail.com>
2020-05-17 19:48:58 +0200

    changing-home-to-class

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import Products from "./Products"
+import Data from "../data/products.json"
 
-export default function Homepage() {
-    return (
+export default class Homepage extends React.Component  {
+    
+    render(){
+
+           return (
         <div>
             <h1>homePage</h1>
             <Link to="./Products" >
-            products
+            <h1>products</h1>
+            {Data.map((item)=>{
+                console.log(item, "item")
+            })}
             </Link>
         </div>
     )
+    }
 }
